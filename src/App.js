0e94864dd638bb04ff4b080226e3be25c9f9a896
Safvan Avcı <43886857+safvanavci@users.noreply.github.com>
2022-11-24 12:21:21 +0300

    Not found page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Category from "./pages/Category";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/*" element={<NotFound/>}/>
         <Route path="category/:name" element={<Category/>}/>
       </Routes>
       <div></div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center py-5">
+      <a href="/">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/error/title._TTD_.png"
+          alt=""
+        />
+      </a>
+      <a href="https://www.aboutamazon.com/news/workplace/how-much-does-amazon-love-dogs-just-ask-one-of-the-7-000-pups-that-work-here?utm_source=gateway&utm_medium=404">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/error/126._TTD_.jpg"
+          alt=""
+        />
+      </a>
+    </div>
+  );
+}
