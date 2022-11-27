fa4606b12a6c8451dd8bea26cf608d7d129f56d9
Safvan Avcı <43886857+safvanavci@users.noreply.github.com>
2022-11-27 17:23:42 +0300

    section update

--- src/components/home/sections/Auto.js
+++ src/components/home/sections/Auto.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Spotlight() {
-
   const [datas, setDatas] = useState([]);
-  
+
   useEffect(() => {
     axios
       .get("https://dummyjson.com/products/category/automotive")
@@ -17,13 +17,15 @@ export default function Spotlight() {
       <div className="w-full overflow-x-auto">
         <div className="flex w-fit gap-20">
           {datas?.map((data) => (
-            <div className="w-[200px] h-[200px] " key={data.id}>
-              <img
-                src={data.thumbnail}
-                alt=""
-                className="w-full h-full object-contain"
-              />
-            </div>
+            <Link to={`/product/${data.id}`}>
+              <div className="w-[200px] h-[200px] " key={data.id}>
+                <img
+                  src={data.thumbnail}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
